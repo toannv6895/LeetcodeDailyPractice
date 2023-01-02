a1634310519b5cbe61834f22822df1638c4a4ebf
Toan Nguyen <toan.nguyen@personifyinc.com>
2023-01-02 22:20:35 +0700

    day 2

---
+++ Day2/CharacterReplacement/CharacterReplacement/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterReplacement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(CharacterReplacement("ABAB",2));
+            //Console.WriteLine(CharacterReplacement("ABAA", 0));
+            Console.WriteLine(CharacterReplacement("ABBB", 2));
+        }
+
+        //I'm wrong with this solution
+        public static int CharacterReplacement(string s, int k)
+        {
+            //Check validate
+            if (s == null) return 0;
+            if (s.Length < 2) return s.Length;
+
+            int l = 0;
+            int r = 1;
+            int curr = 1;
+            int max = 0;
+            int k1 = k;
+
+            while (l < r && r < s.Length)
+            {
+                if (s[l] != s[r])
+                {
+                    if (k > 0)
+                    {
+                        curr++;
+                        r++;
+                        k--;
+                    }
+                    else
+                    {
+                        curr = 1;
+                        k = k1;
+                        l++;
+                        r = l + 1;
+                    }
+
+                    while (r < s.Length - 1 && s[r] == s[r + 1])
+                    {
+                        curr++;
+                        r++;
+                    }
+                }
+                else
+                {
+                    curr++;
+                    r++;
+                }
+
+                max = Math.Max(max, curr);
+            }
+
+            return max;
+        }
+    }
+}
---
+++ Day2/CharacterReplacement/CharacterReplacement2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterReplacement2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(CharacterReplacement("ABAB",2));
+            //Console.WriteLine(CharacterReplacement("ABAA", 0));
+            Console.WriteLine(CharacterReplacement("ABBB", 2));
+        }
+
+        public static int CharacterReplacement(string s, int k)
+        {
+            if (s == null) return 0;
+
+            if (s.Length < 2)
+            {
+                return s.Length;
+            }
+
+            int[] arr = new int[26];
+
+            int maxCount = 0;
+            int res = 0;
+            int left = 0;
+            for (int right = 0; right < s.Length; right++)
+            {
+                maxCount = Math.Max(maxCount, ++arr[s[right] - 'A']);
+                while (right - left + 1 - maxCount > k)
+                {
+                    arr[s[left] - 'A']--;
+                    left++;
+                }
+                res = Math.Max(res, right - left + 1);
+            }
+
+            return res;
+        }
+    }
+}
