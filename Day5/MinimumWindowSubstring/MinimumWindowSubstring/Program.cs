c5d295c16c87c1604a9a70c721995afdf18aa8a1
Toan Nguyen <toan.nguyen@personifyinc.com>
2023-01-05 22:58:59 +0700

    day 5

---
+++ Day5/MinimumWindowSubstring/MinimumWindowSubstring/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumWindowSubstring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = "ADOBECODEBANC", t = "ABC";
+            //string s = "ab", t = "a";
+            //string s = "ab", t = "b";
+            string s = "bbaa", t = "aba";
+            Console.WriteLine(MinWindow(s, t));
+            Console.ReadKey();
+        }
+
+        // This is my first solution => Failed
+        public static string MinWindow(string s, string t)
+        {
+            if (s == null || t == null)
+            {
+                return "";
+            }
+
+            if (s == t)
+            {
+                return s;
+            }
+
+            if (s.Length < t.Length)
+            {
+                return "";
+            }
+
+            Queue<int> ints = new Queue<int>();
+            StringBuilder stringBuilder= new StringBuilder();
+            string result = string.Empty;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                stringBuilder.Append(s[i]);
+                string temp = stringBuilder.ToString();
+
+                if (t.Contains(s[i]))
+                {
+                    ints.Enqueue(i);
+                }
+
+                while ((IsContains(temp, t) || temp == t))
+                {
+                    if (result.Length > temp.Length || result == string.Empty)
+                    {
+                        result = temp;
+                    }
+
+                    //if (ints.Count >= 2)
+                    //{
+                    //    ints.Dequeue();
+                    //}
+
+                    if (ints.Any())
+                    {
+                        int index = ints.Dequeue();
+                        temp = s.Substring(index, i - index + 1);
+                        stringBuilder = new StringBuilder(temp);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static bool IsContains(string a, string b)
+        {
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (!a.Contains(b[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Day5/MinimumWindowSubstring/MinimumWindowSubstring2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumWindowSubstring2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = "ADOBECODEBANC", t = "ABC";
+            //string s = "ab", t = "a";
+            //string s = "ab", t = "b";
+            //string s = "bbaa", t = "aba";
+            string s = "ab", t = "A";
+            Console.WriteLine(MinWindow(s, t));
+            Console.ReadKey();
+        }
+
+        public static string MinWindow(string s, string t)
+        {
+            if (s == null || t == null)
+            {
+                return "";
+            }
+
+            if (s.Length < 2)
+            {
+                if (s == t)
+                {
+                    return s;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+
+            if (s.Length < t.Length)
+            {
+                return "";
+            }
+
+            //Dùng để chứa danh sách index các phần tử đã được tìm thấy
+            Queue<int> ints = new Queue<int>();
+            int l = 0;
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            Dictionary<char, int> mapCheck = new Dictionary<char, int>();
+            int min = -1, max = -1;
+
+            for (int i = 0; i < t.Length; i++)
+            {      
+                if (!mapCheck.ContainsKey(t[i]))
+                {
+
+                    mapCheck[t[i]] = 1;
+                }
+                else
+                {
+                    mapCheck[t[i]]++;
+                }
+
+                map[t[i]] = 0;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (map.ContainsKey(s[i]))
+                {
+                    map[s[i]]++;
+                    ints.Enqueue(i);
+                }
+
+                while (CompareDictionary(map, mapCheck))
+                {
+                    int min_temp = ints.Dequeue();
+                    int max_temp = i;
+
+                    if (max_temp - min_temp < max - min || (min < 0 && max < 0))
+                    {
+                        max = max_temp;
+                        min = min_temp;
+                    }
+
+                    map[s[min_temp]]--;
+                }
+            }
+
+            if (min == -1 && max == -1)
+            {
+                return "";
+            }
+
+            return s.Substring(Math.Max(min,0), Math.Max(max - min + 1,0));
+        }
+
+        private static bool CompareDictionary(Dictionary<char, int> a, Dictionary<char, int> b)
+        {
+            foreach (var item in a.Keys)
+            {
+                if (a[item] < b[item])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
