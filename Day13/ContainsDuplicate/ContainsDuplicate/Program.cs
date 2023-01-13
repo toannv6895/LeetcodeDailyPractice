f355e50a55910cb7da86291ef2d8824b41af9b47
Toan Nguyen <toan.nguyen@personifyinc.com>
2023-01-13 21:26:51 +0700

    day 13 - 3

---
+++ Day13/ContainsDuplicate/ContainsDuplicate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainsDuplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+            Console.WriteLine(ContainsDuplicate(ints));
+            Console.ReadLine();
+        }
+
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                {
+                    return true;
+                }
+
+                dic[nums[i]] = 1;
+            }
+
+            return false;
+        }
+    }
+}
