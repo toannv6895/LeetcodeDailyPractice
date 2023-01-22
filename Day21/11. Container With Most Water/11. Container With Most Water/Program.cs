007c8fda580e3a2c6e8a2b301c8132bbd0636d82
Toan Nguyen <toan.nguyen@personifyinc.com>
2023-01-22 20:39:05 +0700

    day 21

---
+++ Day21/11. Container With Most Water/11. Container With Most Water/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Container_With_Most_Water
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] nums = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            int[] nums = { 1, 1 };
+            Console.WriteLine(MaxArea(nums));
+            Console.ReadLine();
+        }
+
+        public static int MaxArea(int[] height)
+        {
+            int n = height.Length;
+            int left = 0;
+            int right = n - 1;
+            int max = 0;
+
+            if (height == null || height.Length < 2)
+            {
+                return 0;
+            }
+
+            while (left < right)
+            {
+                max = Math.Max(max, (right - left) * Math.Min(height[left], height[right]));
+
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return max;
+        }
+    }
+}
