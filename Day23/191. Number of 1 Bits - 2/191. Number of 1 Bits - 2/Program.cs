aa4649d23a3a52867e15d93b7236b6a97a1d5c9a
Toan Nguyen <toan.nguyen@personifyinc.com>
2023-01-24 22:55:46 +0700

    day 32 -solution 2

--- Day23/191. Number of 1 Bits - 2/191. Number of 1 Bits - 2/Program.cs
+++ Day23/191. Number of 1 Bits - 2/191. Number of 1 Bits - 2/Program.cs
@@ -19,19 +19,12 @@ namespace _191.Number_of_1_Bits
         {
             uint mask = 1;
             int count = 0;
-            uint num = 0;
-            //First time I have take res =1
-            //Now res & n gives me last digit of res & 1 
-            //now I am left shifting res by 1 everytime and others are 0 in res
-            //now that digit which is 1 in res with that position in n will give me 1 or 0
-            //but we know others are 0 in res so answer will always be 0 for other position
-            //so if that position of 1 in res and position in n gives 1 then result will be !=0
-            //hence increase the counter
             for (int i = 1; i <= 32; i++)
             {
-                num = mask & n;
-                if (num != 0)
+                if ((mask & n) != 0)
+                {
                     count++;
+                }
 
                 mask = mask << 1;
             }
