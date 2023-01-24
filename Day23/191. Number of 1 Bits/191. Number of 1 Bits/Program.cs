b6310274d9debb84ef873c83454c3fb9a07d7a11
Toan Nguyen <toan.nguyen@personifyinc.com>
2023-01-24 21:45:16 +0700

    this is first idea, but i don't know why it's wrong answer

---
+++ Day23/191. Number of 1 Bits/191. Number of 1 Bits/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _191.Number_of_1_Bits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint nums = 00000000000000000000000000001011;
+            Console.WriteLine(HammingWeight(nums));
+            Console.ReadLine();
+        }
+
+        public static int HammingWeight(uint n)
+        {
+            string s = n.ToString();
+            int result = 0;
+            foreach (char c in s)
+            {
+                if (c == '1')
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
