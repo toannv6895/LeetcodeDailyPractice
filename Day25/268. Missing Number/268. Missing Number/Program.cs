bd64470e2f7ae8de4edf2871f813340d9f1dfe4d
Toan Nguyen <toan.nguyen@personifyinc.com>
2023-01-26 20:46:03 +0700

    day 25 - first solution

---
+++ Day25/268. Missing Number/268. Missing Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _268_Missing_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 0, 1 };
+            Console.WriteLine(MissingNumber(nums));
+            Console.ReadLine();
+        }
+
+        public static int MissingNumber(int[] nums)
+        {
+            int sum = (nums.Length * (nums.Length + 1)) / 2;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum -= nums[i];
+            }
+            return sum;
+        }
+    }
+}
